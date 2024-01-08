a707a42ceb8dd8efd015ede9d73df22e9e8db22b
jieun <lesson0186@gmail.com>
2024-01-08 23:02:38 +0900

    매개변수 전 까지

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,24 +1,36 @@
 export function printOwing(invoice) {
-  let outstanding = 0;
+  printBanner()
+  let outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+  printDetails(invoice, outstanding);
+}
 
+function printBanner() {
   console.log('***********************');
   console.log('**** Customer Owes ****');
   console.log('***********************');
+}
 
-  // calculate outstanding
+function calculateOutstanding(invoice) {
+  //return invoice.orders.reduce((sum, order) => sum + order.amount, 0)
+
+  let result = 0;
   for (const o of invoice.orders) {
-    outstanding += o.amount;
+    result += o.amount;
   }
+  return result;
+}
 
-  // record due date
+function recordDueDate(invoice) {
   const today = new Date();
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
   );
+}
 
-  //print details
+function printDetails(invoice, outstanding) {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
--- ch6/6-5-3.js
+++ ch6/6-5-3.js
@@ -1,3 +1,4 @@
+//전체 객체를 받고있다. state만 받아오도록 수정
 export function inNewEngland(aCustomer) {
   return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
 }
